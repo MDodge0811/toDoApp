d3709e2d0d03c2e33cd0efc61563ca15a7144f94
Malachi Dodge <kaid@pop-os.localdomain>
2020-11-24 09:15:27 -0500

    refactored to include localStorage

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 const remindersList = document.getElementById('reminders-list');
 
+// Creates an individual reminder and applies CSS markers.
+const createReminder = (id, reminderText) => {
+	const node = document.createElement('div');
+	node.classList.add('single-reminder');
+	node.id = id;
+	node.innerHTML = /*html*/ `
+		<p>${reminderText}</p>
+		<div>
+			<button class='complete btn fas fa-check'></button>
+			<button class='delete btn fas fa-trash-alt'></button>
+		</div>
+	`;
+	remindersList.appendChild(node);
+
+	const singleReminderList = document.getElementsByClassName('single-reminder');
+	for (const [i, value] of Array.from(singleReminderList).entries()) {
+		if (i % 2 == 0) {
+			value.classList.add('alt');
+		}
+	}
+};
+
+// checks localStorage and builds stored reminders on page load.
+const buildPage = () => {
+	for (let reminder in localStorage) {
+		if (reminder.startsWith('#'))
+			createReminder(reminder, localStorage.getItem(reminder));
+	}
+};
+buildPage();
+
+// Event Listeners
+
+// Listens for action on existing reminders
 remindersList.addEventListener('click', (event) => {
+	const singleReminder = event.target.closest('.single-reminder');
 	if (event.target.matches('.delete')) {
-		event.target.closest('.single-reminder').remove();
+		singleReminder.remove();
+		localStorage.removeItem(singleReminder.id);
+		event.target.closest('.single-reminder');
 	} else if (event.target.matches('.complete')) {
-		event.target.closest('.single-reminder').classList.add('completed');
+		singleReminder.classList.add('completed');
 		event.target.remove();
+		localStorage.removeItem(singleReminder.id);
 	}
 });
 
+// Listens for creation of new reminders.
 document.querySelector('#create').addEventListener('click', (event) => {
 	event.preventDefault();
 
@@ -16,83 +55,8 @@ document.querySelector('#create').addEventListener('click', (event) => {
 	if (reminder.value.trim() === '') {
 		reminder.placeholder = 'Forget something?';
 	} else {
-		const node = document.createElement('div');
-		node.classList.add('single-reminder');
-		node.innerHTML = /*html*/ `
-		<p>${reminder.value}</p>
-		<div>
-			<button class='complete btn fas fa-check'></button>
-			<button class='delete btn fas fa-trash-alt'></button>
-		</div>
-	`;
-
-		remindersList.appendChild(node);
-
-		const singleReminderList = document.getElementsByClassName(
-			'single-reminder'
-		);
-		for (const [i, value] of Array.from(singleReminderList).entries()) {
-			if (i % 2 == 0) {
-				value.classList.add('alt');
-			}
-		}
+		const id = `#${Math.floor(Math.random() * 1000000)}`;
+		localStorage.setItem(id, reminder.value);
+		createReminder(id, reminder.value);
 	}
 });
-
-// Version 2
-// const remindersList = document.getElementById('reminders-list');
-// let n = 0;
-
-// document.querySelector('#create').addEventListener('click', (event) => {
-// 	event.preventDefault();
-
-// 	const reminder = document.querySelector('#reminder').value;
-// 	const node = document.createElement('div');
-// 	node.classList.add('single-reminder');
-// 	node.innerHTML = `
-// 		<p>${reminder}</p>
-// 		<div>
-// 			<button class='complete' id='complete${n}'>Y</button>
-// 			<button class='delete' id='delete${n}'>X</button>
-// 		</div>
-// 	`;
-
-// 	remindersList.appendChild(node);
-
-// 	const complete = document.querySelector(`#complete${n}`);
-// 	complete.addEventListener('click', () => {
-// 		complete.parentNode.parentNode.remove();
-// 		console.log('completed!!');
-// 	});
-// 	const remove = document.querySelector(`#delete${n}`);
-// 	remove.addEventListener('click', () => remove.parentNode.parentNode.remove());
-
-// 	n++;
-// });
-
-// Version 1
-// document.querySelector('#create').addEventListener('click', (event) => {
-// 	event.preventDefault();
-
-// 	const reminder = document.querySelector('#reminder').value;
-
-// 	const reminderText = document.createElement('p');
-// 	reminderText.innerText = reminder;
-
-// 	const remove = document.createElement('button');
-// 	remove.classList.add('remove');
-// 	remove.innerText = 'X';
-// 	remove.addEventListener('click', () => remove.parentNode.remove());
-
-// 	const complete = document.createElement('button');
-// 	remove.classList.add('remove');
-// 	complete.innerText = 'Y';
-// 	complete.addEventListener('click', () => complete.parentNode.remove());
-// 	const node = document.createElement('div');
-// 	node.classList.add('single-reminder');
-
-// 	remindersList.appendChild(node);
-// 	node.appendChild(reminderText);
-// 	node.appendChild(complete);
-// 	node.appendChild(remove);
-// });
